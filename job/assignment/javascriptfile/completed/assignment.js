ef01d57c12bb3e7cfdad843c0914d5a249a586a7
rishabasija <77782792+rishabasija@users.noreply.github.com>
2021-01-21 17:22:33 +0530

    Add files via upload

---
+++ job/assignment/javascriptfile/completed/assignment.js
@@ -0,0 +1,14 @@
+objectValue = document.querySelectorAll(".views-field-nothing .field-content p font font")[0].innerHTML;
+console.log("objectValue");
+
+//Publication Value
+publicationValue = document.querySelectorAll(".lbl-licitacao font font")[1].innerHTML;
+console.log("publicationValue");
+
+//Bidding Value
+biddingValue = document.querySelectorAll(".lbl-licitacao font font")[9].innerHTML;
+console.log("biddingValue");
+
+//Link address
+linkAddress = document.querySelectorAll(".field .field__item a")[0].getAttribute("href");
+console.log("linkAddress");
